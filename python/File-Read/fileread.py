374c7cbf8a79e62ea261295c3da51325f231551c
jerardh <jerardhtomj@gmail.com>
2025-01-02 19:09:14 +0530

    Read CSV and print

--- python/File-Read/fileread.py
+++ python/File-Read/fileread.py
@@ -1,6 +1,10 @@
-file=open("sample.txt")
-lines=file.readlines()
+file=open("data.csv")
+rows=file.readlines()
 file.close()
 print("Lines in file are")
-for line in lines:
-    print(line)
+for line in rows:
+    cols=line.split(",")
+    for data in cols:
+        print(data,end=" | ")
+    
+    
